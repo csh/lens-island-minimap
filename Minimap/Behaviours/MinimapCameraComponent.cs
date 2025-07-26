c563a360f8ac9029cf1b8c9b22e8e0c2b9b748d4
csh <csh@smrkn.com>
2025-07-26 18:05:16 +0100

    Add support for stylised overlays
    Add a terrible default overlay
    
    Add smooth zoom

--- Minimap/Behaviours/MinimapCameraComponent.cs
+++ Minimap/Behaviours/MinimapCameraComponent.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using Flow.StatusEffects;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Minimap.Behaviours;
@@ -7,16 +9,22 @@ namespace Minimap.Behaviours;
 public class MinimapCameraComponent : MonoBehaviour
 {
     private static readonly Vector2 MinimapSize = new(160f, 160f);
+    private static readonly Vector2 OverlaySize = new(250f, 250f);
     private static bool RotateWithPlayer => MinimapPlugin.Instance.RotateWithPlayer.Value;
-    
+
+    private const float MinCameraHeight = 30f;
+    private const float MaxCameraHeight = 100f;
+    private const float ZoomStep = 10f;
+
+    private float _cameraHeightTarget = 60f;
     private float _cameraHeight = 60f;
 
-    private Camera _minimapCamera;
     private RenderTexture _minimapRenderTexture;
-    private RawImage _minimapImage;
-    private RectTransform _minimapRect;
-    private GameObject _minimapRoot;
+    private GameObject _minimapContainer;
     private Transform _cameraTransform;
+    private GameObject _overlayRoot;
+    private RawImage _minimapImage;
+    private Camera _minimapCamera;
 
     private void Start()
     {
@@ -38,46 +46,129 @@ public class MinimapCameraComponent : MonoBehaviour
 
     private void CreateMinimapUI()
     {
-        var compass = FindObjectOfType<CompassSpinner>();
+        // TODO: Add support for toggling replacement of the vanilla compass
+        
+        var compass = FindObjectOfType<CompassSpinner>(true);
         if (!compass)
         {
             MinimapPlugin.Logger.LogInfo("Failed to find compass for UI injection");
             return;
         }
 
-        _minimapRoot = new GameObject("Minimap");
-        _minimapRoot.transform.SetParent(compass.transform.parent, false);
-        _minimapRect = _minimapRoot.AddComponent<RectTransform>();
-        _minimapRect.sizeDelta = MinimapSize;
-        _minimapRect.anchorMin = new Vector2(0f, 1f);
-        _minimapRect.anchorMax = new Vector2(0f, 1f);
-        _minimapRect.pivot = new Vector2(0f, 1f);
-        _minimapRect.anchoredPosition = new Vector2(60f, -200f);
+        var statusEffects = FindObjectOfType<StatusEffectSlots>();
+        if (statusEffects)
+        {
+            statusEffects.transform.localPosition = statusEffects.transform.localPosition with
+            {
+                x = 20f
+            };
+        }
 
-        var maskImage = _minimapRoot.AddComponent<Image>();
+        compass.gameObject.SetActive(false);
+
+        _minimapContainer = new GameObject("MinimapContainer");
+        _minimapContainer.transform.SetParent(compass.transform.parent, false);
+        var containerRect = _minimapContainer.AddComponent<RectTransform>();
+        containerRect.sizeDelta = OverlaySize;
+        containerRect.anchorMin = new Vector2(0f, 1f);
+        containerRect.anchorMax = new Vector2(0f, 1f);
+        containerRect.pivot = new Vector2(0f, 1f);
+        containerRect.anchoredPosition = new Vector2(40f, -200f);
+
+        _overlayRoot = new GameObject("MinimapRotatingRoot");
+        _overlayRoot.transform.SetParent(_minimapContainer.transform, false);
+        var rotatingRect = _overlayRoot.AddComponent<RectTransform>();
+        rotatingRect.sizeDelta = OverlaySize;
+        rotatingRect.anchorMin = new Vector2(0.5f, 0.5f);
+        rotatingRect.anchorMax = new Vector2(0.5f, 0.5f);
+        rotatingRect.pivot = new Vector2(0.5f, 0.5f);
+        rotatingRect.anchoredPosition = Vector2.zero;
+
+        var minimapRoot = new GameObject("Minimap");
+        minimapRoot.transform.SetParent(_overlayRoot.transform, false);
+        var minimapRect = minimapRoot.AddComponent<RectTransform>();
+        minimapRect.sizeDelta = MinimapSize;
+        minimapRect.anchorMin = new Vector2(0.5f, 0.5f);
+        minimapRect.anchorMax = new Vector2(0.5f, 0.5f);
+        minimapRect.pivot = new Vector2(0.5f, 0.5f);
+        minimapRect.anchoredPosition = Vector2.zero;
+
+        var maskImage = minimapRoot.AddComponent<Image>();
         maskImage.sprite = CreateCircleSprite((int)MinimapSize.x);
 
-        var mask = _minimapRoot.AddComponent<Mask>();
+        var mask = minimapRoot.AddComponent<Mask>();
         mask.showMaskGraphic = false;
 
         var imageObj = new GameObject("MinimapImage");
-        imageObj.transform.SetParent(_minimapRoot.transform, false);
+        imageObj.transform.SetParent(minimapRoot.transform, false);
         _minimapImage = imageObj.AddComponent<RawImage>();
         var imageRect = imageObj.GetComponent<RectTransform>();
         imageRect.anchorMin = Vector2.zero;
         imageRect.anchorMax = Vector2.one;
         imageRect.offsetMin = Vector2.zero;
         imageRect.offsetMax = Vector2.zero;
+
+        var overlayImageObj = new GameObject("MinimapOverlayImage");
+        overlayImageObj.transform.SetParent(_overlayRoot.transform, false);
+        var overlayImage = overlayImageObj.AddComponent<Image>();
+        overlayImage.sprite = LoadSpriteFromFile(@"H:\Minimap\Border\Border.png");
+
+        var overlayImageRect = overlayImageObj.GetComponent<RectTransform>();
+        overlayImageRect.sizeDelta = OverlaySize;
+        overlayImageRect.anchorMin = new Vector2(0.5f, 0.5f);
+        overlayImageRect.anchorMax = new Vector2(0.5f, 0.5f);
+        overlayImageRect.pivot = new Vector2(0.5f, 0.5f);
+        overlayImageRect.anchoredPosition = Vector2.zero;
+    }
+
+    private static Sprite LoadSpriteFromFile(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            Debug.LogError($"File not found: {filePath}");
+            return null;
+        }
+
+        var fileData = File.ReadAllBytes(filePath);
+        var texture = new Texture2D((int)OverlaySize.x, (int)OverlaySize.y);
+
+        if (texture.LoadImage(fileData))
+        {
+            return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+        }
+
+        MinimapPlugin.Logger.LogError("Failed to load image data");
+        Destroy(texture);
+        return null;
+    }
+
+    private void HandleZoomInput()
+    {
+        if (PauseMenu.Instance && PauseMenu.Instance.IsVisible) return;
         
-        // TODO: Add border overlay
+        // TODO: Make keybinds configurable
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            _cameraHeightTarget = Mathf.Min(_cameraHeightTarget + ZoomStep, MaxCameraHeight);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            _cameraHeightTarget = Mathf.Max(_cameraHeightTarget - ZoomStep, MinCameraHeight);
+        } 
+    }
+
+    private void SmoothZoom()
+    {
+        _cameraHeight = Mathf.MoveTowards(_cameraHeight, _cameraHeightTarget, 40f * Time.deltaTime);
     }
 
     private void CreateMinimapCamera()
     {
-        _minimapRenderTexture = new RenderTexture((int)MinimapSize.x, (int)MinimapSize.y, 16, RenderTextureFormat.ARGB32)
-        {
-            name = "MinimapRT"
-        };
+        _minimapRenderTexture =
+            new RenderTexture((int)MinimapSize.x, (int)MinimapSize.y, 16, RenderTextureFormat.ARGB32)
+            {
+                name = "MinimapRT"
+            };
         _minimapRenderTexture.Create();
 
         var camObj = new GameObject("MinimapCamera");
@@ -107,15 +198,8 @@ public class MinimapCameraComponent : MonoBehaviour
     {
         if (!_cameraTransform || !_minimapCamera) return;
 
-        // TODO: Make keybinds configurable, clamp ranges, smooth zoom step.
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            _cameraHeight -= 10;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha9))
-        {
-            _cameraHeight += 10;
-        }
+        HandleZoomInput();
+        SmoothZoom();
 
         var playerPos2D = Player.PlayerPos2D;
         _minimapCamera.transform.position = new Vector3(playerPos2D.x, _cameraHeight, playerPos2D.y);
@@ -124,11 +208,11 @@ public class MinimapCameraComponent : MonoBehaviour
         if (RotateWithPlayer)
         {
             var yaw = _cameraTransform.eulerAngles.y;
-            _minimapImage.rectTransform.localEulerAngles = new Vector3(0f, 0f, yaw);
+            _overlayRoot.transform.localEulerAngles = new Vector3(0f, 0f, yaw - 45f);
         }
         else
         {
-            _minimapImage.rectTransform.localEulerAngles = Vector3.zero;
+            _overlayRoot.transform.localEulerAngles = Vector3.zero;
         }
     }
 
@@ -162,6 +246,21 @@ public class MinimapCameraComponent : MonoBehaviour
 
     private void OnDestroy()
     {
+        var compass = FindObjectOfType<CompassSpinner>();
+        if (compass)
+        {
+            compass.gameObject.SetActive(true);
+        }
+
+        var statusEffects = FindObjectOfType<StatusEffectSlots>();
+        if (statusEffects)
+        {
+            statusEffects.transform.localPosition = statusEffects.transform.localPosition with
+            {
+                x = 100f
+            };
+        }
+
         if (_minimapRenderTexture != null)
         {
             _minimapRenderTexture.Release();
@@ -173,9 +272,9 @@ public class MinimapCameraComponent : MonoBehaviour
             Destroy(_minimapCamera.gameObject);
         }
 
-        if (_minimapRoot != null)
+        if (_minimapContainer != null)
         {
-            Destroy(_minimapRoot);
+            Destroy(_minimapContainer); // Destroy container (which destroys overlay and minimap too)
         }
     }
 }
